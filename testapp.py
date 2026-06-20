9b2adb6a40104b00ebe31e16dab633c3e3b5b5e5
Louka Abed <louka.abed@yahoo.com>
2026-06-20 04:53:20 +0300

    Update page titles for analytics and streamlit

--- testapp.py
+++ testapp.py
@@ -4,11 +4,11 @@ st.set_page_config(page_title="My App")
 
 # Define your pages
 home_page = st.Page("hello.py", title="Home", icon="🏠")
-another_page = st.Page("analytics.py", title="Analytics", icon="📊")
-third_page = st.Page("hellostreamlit.py", title="Hello Streamlit", icon="👋")
+another_page = st.Page("analytics.py", title="test", icon="📊")
+third_page = st.Page("hellostreamlit.py", title="test", icon="👋")
 fourth_page = st.Page('sas.py', title='SAS Experience', icon='👋')
 # Group pages and initialize navigation
 # Run navigation
-nav = st.navigation([home_page])
+nav = st.navigation([home_page, third_page])
 
 nav.run()
