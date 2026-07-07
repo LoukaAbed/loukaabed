3ceff699b27420e87c6df498dcd4170c6b488117
Louka Abed <louka.abed@yahoo.com>
2026-07-07 08:36:16 +0300

    Change contact page icon
    Updated contact page icon from '📧' to '📬'.

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ st.set_page_config(
 )
 
 # Define page navigation
-contact = st.Page("contact.py", title="Contact", icon="📧")
+contact = st.Page("contact.py", title="Contact", icon="📬")
 p1 = st.Page("p1.py", title="Project 1", icon="📈")
-# Run navigation without executing any background connection logic here
+# Run navigation 
 nav = st.navigation([contact, p1])
 nav.run()
