a3550ce8008c18bb9035dfb9e0521fc28fbf7d86
Louka Abed <louka.abed@yahoo.com>
2026-07-07 07:35:58 +0300

    Update and rename analytics.py to p1.py

--- analytics.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-st.set_page_config(layout="wide")
-
-st.title("📊 Clinical Trial Data Engineering Pipeline")
-st.write("code samples")
---
+++ p1.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(layout="wide")
+
+st.title("📈 Project 1")
+st.write("Project 1 goes here")
