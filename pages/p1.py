cf657fda54527157881e6e8421cfba8ea97b5191
loukaabed <louka.abed@yahoo.com>
2026-07-11 06:47:05 +0300

    commit

--- pages/p1.py
+++ pages/p1.py
@@ -7,15 +7,15 @@ st.divider()
 
 if 'uploaded_file' not in st.session_state:
     st.session_state['active_tbl'] = None
+    st.session_state['deleted_file']=False
 
 uploaded_file = st.file_uploader("Upload a CSV file: Max Size 2MB", type=["csv"])
 if uploaded_file is not None:
     if uploaded_file.size > 2 * 1024 * 1024:
         st.error("File size exceeds the maximum limit of 2MB.")
         st.stop()
-    if st.session_state['active_tbl'] is None:
+    if st.session_state['active_tbl'] is None and not st.session_state['deleted_file']:
         st.session_state['active_tbl'] = db.store_db(uploaded_file)
-        st.session_state['active_tbl'] = st.session_state['active_tbl']
         st.success(f"Your file {uploaded_file.name.replace(' ', '_')} was uploaded and stored in database as: {st.session_state['active_tbl']}")
 if st.session_state['active_tbl'] is not None:
     uploadedfile_preview = f"SELECT * FROM {st.session_state['active_tbl']} LIMIT 5"
@@ -24,6 +24,7 @@ if st.button("Drop Uploaded Table"):
     db.drop_db(st.session_state['active_tbl'])
     st.success(f"Table {st.session_state['active_tbl']} has been dropped from the database.")
     st.session_state['active_tbl'] = None
+    st.session_state['deleted_file']=True
     st.rerun()
 
 st.divider()
