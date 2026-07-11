667bd88e52774a5ac119c949d6a91c8af69db09d
loukaabed <louka.abed@yahoo.com>
2026-07-11 05:12:49 +0300

    commit

--- pages/p1.py
+++ pages/p1.py
@@ -1,8 +1,20 @@
+from pydoc import text
 import streamlit as st
 import utils.db_utils as db
 
 st.title("Neon Database Connection Test")
 st.divider()
+
+uploaded_file = st.file_uploader("Upload a CSV file: Max Size 2MB", type=["csv"])
+if uploaded_file is not None:
+    if uploaded_file.size > 2 * 1024 * 1024:
+        st.error("File size exceeds the maximum limit of 2MB.")
+        st.stop()
+    tbl_name = db.store_db(uploaded_file)
+    st.success(f"Your file {uploaded_file.name} was uploaded and stored in database as: {tbl_name}")
+    uploadedfile_preview = f"SELECT * FROM {tbl_name} LIMIT 5"
+    st.write(db.fetch_db(uploadedfile_preview))
+    
 
 query = "SELECT * FROM bp where gender = :gender and age > :age"
 st.write(db.fetch_db(query, {"gender": "Male", "age": 25}))
--- utils/db_utils.py
+++ utils/db_utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import uuid
 from sqlalchemy import create_engine, text
 
 #build connection to the database
@@ -14,3 +15,13 @@ def edit_db(query, query_dic=None):
     with bridge.begin() as conn:
         result = conn.execute(text(query), query_dic)
         return pd.DataFrame(result.mappings())   
+
+def name_db():
+    return 'user_' + uuid.uuid4().hex
+
+def store_db(uploaded_file):
+    df = pd.read_csv(uploaded_file)
+    tbl_name = name_db()
+    with bridge.begin() as conn:
+        df.to_sql(tbl_name, con=conn, if_exists='replace', index=False)
+    return tbl_name
