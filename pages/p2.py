df664de2b802962f80fe55c721598db9bacce04b
loukaabed <louka.abed@yahoo.com>
2026-07-12 09:08:17 +0300

    commit

--- pages/p2.py
+++ pages/p2.py
@@ -8,7 +8,7 @@ st.title('Upload Dataset')
 st.divider()
 schema_name=st.text_input("New DB Schema:", placeholder="db1")
 if st.button('Create New Schema'):
-    if schema_name.strip:
+    if schema_name.strip():
         safe_name=db.name_db(tbl_name=schema_name, prefix='', name_type='file')
         st.success(f'Schema name after format: {safe_name}')
     else:
