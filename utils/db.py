d5d28ae803fada79f5a754bd876ac5a5c24d1bed
loukaabed <louka.abed@yahoo.com>
2026-07-12 03:36:04 +0300

    commit

--- utils/db.py
+++ utils/db.py
@@ -3,6 +3,7 @@ import os
 import uuid
 from sqlalchemy import create_engine, text
 from typing import Literal
+import re
 
 #build connection to the database options pool_pre_ping, pool_recycle prevent db crash on startup  
 bridge = create_engine(os.environ.get("NEON_DB_URL"), echo=True, pool_pre_ping=True, pool_recycle=300 )
@@ -17,14 +18,25 @@ def edit_db(query, query_dic=None):
         result = conn.execute(text(query), query_dic)
         return pd.DataFrame(result.mappings())   
 
-def name_db(prefix):
-    return prefix + uuid.uuid4().hex
+def name_db(prefix='user_', tbl_name='uuid'):
+    if tbl_name != 'uuid':
+        if '.' in tbl_name:
+            file_name, extension = tbl_name.rsplit('.', 1)
+            extension = extension.lower()
+        else:
+            file_name=tbl_name
+            extension=''
+        return f"{prefix}{re.sub(r'[_]+', '_', re.sub(r'[^a-zA-Z0-9_]', '_', file_name)).lower()}_{extension}"
 
-def store_db(uploaded_file, prefix='user_', if_tbl_exist: Literal['append', 'replace', 'fail']='replace', destination_schema='public'):
+    else:
+        return prefix + uuid.uuid4().hex
+
+
+def store_db(uploaded_file, prefix='user_', tbl_name='uuid', if_tbl_exist: Literal['append', 'replace', 'fail']='replace', destination_schema='public'):
     df = pd.read_csv(uploaded_file)
-    tbl_name = name_db(prefix)
+    safe_tbl_name = name_db(prefix, tbl_name)
     with bridge.begin() as conn:
-        df.to_sql(tbl_name, con=conn, schema=destination_schema, if_exists=if_tbl_exist, index=False)
+        df.to_sql(safe_tbl_name, con=conn, schema=destination_schema, if_exists=if_tbl_exist, index=False)
     return tbl_name
 
 def drop_db(tbl_name):
