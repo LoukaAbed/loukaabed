a5f00617ed8fb92ffb7e3c47030a6d27fa62c2c8
loukaabed <louka.abed@yahoo.com>
2026-07-13 21:37:48 +0300

    commit

--- pages/p3.py
+++ pages/p3.py
@@ -4,4 +4,4 @@ import streamlit as st
 with st.form('batch_files_upload', clear_on_submit=False):
     uploaded=st.file_uploader("Upload multiple files dataset", type=None, accept_multiple_files=True, key='dataset')
     upload_button = st.form_submit_button('Upload Dataset')
-st.write(st.session_state['dataset'])
+st.write(st.session_state['dataset'][0].name)
