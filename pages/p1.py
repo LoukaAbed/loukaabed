ba705610024ebab9e79eb878af25356965ec2e51
loukaabed <louka.abed@yahoo.com>
2026-07-13 03:32:38 +0300

    commit

--- pages/p1.py
+++ pages/p1.py
@@ -18,6 +18,10 @@ st.divider()
 
 st.subheader("User upload file into database, read data, then delete. Will apply data science tools in future iteration. try it!")
 
+if 'saved_tables' not in st.session_state:
+    st.session_state['saved_tables']=None
+    st.session_state['files_count']=0
+
 #using form to prevent db write from random clicks
 with st.form('batch_files_upload', clear_on_submit=False):
     dataset = st.file_uploader("Upload multiple files dataset", type=None, accept_multiple_files=True, key='dataset_key')
@@ -26,16 +30,40 @@ with st.form('batch_files_upload', clear_on_submit=False):
 
 
 
+#saving uploaded files to db
 if upload_button:
     if not dataset:
         st.warning('To upload, please add files first to uploader')
     else:
+        st.session_state['files_count']=len(dataset)
+        #reset upload button
+        st.session_state['saved_tables'] = None
         with st.spinner('Uploading your files to public schema, processing...'):
             tables = db.dataset_db(dataset, schema='public')
             if tables:
-                st.success(f"Originally {len(dataset)} files uploaded and {len(tables)} tables saved successfully in public schema db")
-                for tbl_name, table in tables.items():
-                    st.code(f"Tables Saved to the database: {tbl_name} with  {len(table)} rows")
+                st.session_state['saved_tables']=tables
             else:
                 st.warning("File types are not supported, only accept csv xlsx tsv txt")
+
+#read and delete layer
+if st.session_state['saved_tables']:
+    active_tables=st.session_state['saved_tables']
+    st.success(f"Originally {st.session_state['files_count']} files uploaded and {len(active_tables)} tables saved successfully in public schema db")
+    for tbl_name, table in active_tables.items():
+        st.code(f"Tables Saved to the database: {tbl_name} with  {len(table)} rows")
+
+    st.write("### 📋 Preview uploaded tables")
+    selected_tbl= st.selectbox("Select table for preview", list(active_tables.keys()))
+    st.write(db.fetch_db(f"SELECT * FROM {selected_tbl} LIMIT 5"))
+
+    #Delete selected table
+    if st.button(f"🗑️ Delete {selected_tbl} from Database", type='secondary'):
+        db.drop_db(selected_tbl)
+        del st.session_state['saved_tables'][selected_tbl]
+        if not st.session_state['saved_tables']:
+            st.session_state['saved_tables'] = None
+            st.session_state['files_count'] = 0
+        st.success(f"Table `{selected_tbl}` successfully deleted!")
+        st.rerun()
+
 st.space('large')
