14f3ef207d7a5a95c1d043ae1e681cb8243d7c77
loukaabed <louka.abed@yahoo.com>
2026-07-14 00:26:28 +0300

    commit

--- pages/p3.py
+++ pages/p3.py
@@ -9,7 +9,7 @@ with st.form('batch_files_upload', clear_on_submit=False):
     uploaded=st.file_uploader("Upload multiple files dataset", type=None, accept_multiple_files=True, key=f"{st.session_state['counter']}")
     upload_button = st.form_submit_button('Upload Dataset')
 
-if st.write(st.session_state[f"{st.session_state['counter']}"]):
+if st.session_state['counter']:
     st.write(st.session_state[f"{st.session_state['counter']}"][0].name)
 
 if st.session_state[f"{st.session_state['counter']}"]:
