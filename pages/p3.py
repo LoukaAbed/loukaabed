636f6037e04fb0a0c38f97a7f4b934e47d6c6712
loukaabed <louka.abed@yahoo.com>
2026-07-14 02:45:32 +0300

    commit

--- pages/p3.py
+++ pages/p3.py
@@ -9,9 +9,13 @@ with st.form('batch_files_upload', clear_on_submit=False):
     uploaded=st.file_uploader("Upload multiple files dataset", type=None, accept_multiple_files=True, key=f"{st.session_state['counter']}")
     upload_button = st.form_submit_button('Upload Dataset')
 
-if st.session_state['counter']:
-    st.write(st.session_state[f"{st.session_state['counter']}"][0].name)
-
-if st.session_state[f"{st.session_state['counter']}"]:
-    st.session_state['counter'] +=1
-    st.rerun()
+if upload_button:
+    if not uploaded:
+        st.warning('Please add files to upload first before clicking upload')
+    else:
+        dataset = st.session_state[f"{st.session_state['counter']}"]
+        st.success(st.session_state[f"Your file: {st.session_state['counter']} was successfuly uploaded"][0].name)
+        for file in dataset:
+            st.success(f"Your file: {file.name} was successfuly uploaded")
+        st.session_state['counter'] +=1
+        st.rerun()
