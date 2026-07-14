845dd814d7b2926c276c70a2c9e0551ec63b399d
loukaabed <louka.abed@yahoo.com>
2026-07-14 23:08:49 +0300

    commit

--- pages/p3.py
+++ pages/p3.py
@@ -6,12 +6,13 @@ st.title('Testing Streamlit and code implementation')
 if 'counter' not in st.session_state:
     st.session_state['counter']=0
 with st.form('batch_files_upload', clear_on_submit=True):
-    uploaded=st.file_uploader("Upload multiple files dataset", type=None, accept_multiple_files=True, key='upload')
+    uploaded=st.file_uploader("Upload multiple files dataset", type=None, accept_multiple_files=True, key=f"upload_{st.session_state[counter]}")
     upload_button = st.form_submit_button('Upload Dataset')
 
-if st.session_state['upload']:
-    dataset=st.session_state['upload']
+if not upload_button:
+        st.warning('Please upload the files before clicking submit')
+elif st.session_state[f"upload_{st.session_state[counter]}"]:
+    dataset=st.session_state[f"upload_{st.session_state[counter]}"]
     for file in dataset:
         st.write(f"File: {file.name} was successfully uploaded")
-else:
-    st.warning('Please upload the files before clicking submit')
+    
